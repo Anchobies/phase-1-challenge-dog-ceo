4bf9b191b73ac9ab170a431cdebd408aab611c9f
Anchobies <anthonychung97@gmail.com>
2021-05-24 19:57:35 -0400

    commit tree 32b1833d964a3a40282b14c485d4f415e6c7b1bb

--- src/index.js
+++ src/index.js
@@ -1,1 +1,70 @@
-console.log('%c HI', 'color: firebrick')
+let jsonBreeds;
+
+let addBreeds = breeds => {
+    for (breed of breeds) {
+        let newBreed = document.createElement("li");
+
+        newBreed.textContent = breed;
+
+        document.querySelector("#dog-breeds").append(newBreed);
+
+        newBreed.addEventListener("click", () => {
+            newBreed.style.color = "red";
+        })
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+    const breedUrl = 'https://dog.ceo/api/breeds/list/all';
+
+    fetch(imgUrl)
+    .then(response => response.json())
+    .then(json => {
+        for (url of json.message) {
+            let newImg = document.createElement("img");
+
+            newImg.src = url;
+
+            document.querySelector("#dog-image-container").append(newImg);
+        }
+    });
+
+    fetch(breedUrl)
+    .then(response => response.json())
+    .then(json => {
+        jsonBreeds = Object.keys(json.message);
+        addBreeds(jsonBreeds);
+    });
+
+    document.querySelector("select").addEventListener("change", e => {
+        let filteredBreeds;
+
+        switch (e.target.value) {
+            case "a":
+                filteredBreeds = jsonBreeds.filter(breed => breed[0] === "a");
+
+                document.querySelector("#dog-breeds").innerHTML = "";
+                addBreeds(filteredBreeds);
+                break;
+            case "b":
+                filteredBreeds = jsonBreeds.filter(breed => breed[0] === "b");
+
+                document.querySelector("#dog-breeds").innerHTML = "";
+                addBreeds(filteredBreeds);
+                break;
+            case "c":
+                filteredBreeds = jsonBreeds.filter(breed => breed[0] === "c");
+
+                document.querySelector("#dog-breeds").innerHTML = "";
+                addBreeds(filteredBreeds);
+                break;
+            case "d":
+            filteredBreeds = jsonBreeds.filter(breed => breed[0] === "d");
+
+            document.querySelector("#dog-breeds").innerHTML = "";
+            addBreeds(filteredBreeds);
+            break;
+        }
+    })
+})
